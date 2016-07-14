3ec734f15c03472cb5fa34be7ef129e4adb702ab
kenpcjr <kennethcooke@me.com>
2016-07-14 10:31:30 -0400

    Done.

--- Cards/Cards/ViewController.swift
+++ Cards/Cards/ViewController.swift
@@ -10,10 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
     // TODO: Create IB outlets
+    @IBOutlet weak var upperLeftLabel: UILabel!
+    @IBOutlet weak var centerLabel: UILabel!
+    @IBOutlet weak var lowerRightLabel: UILabel!
+    @IBOutlet weak var fourOfClubsButton: UIButton!
+    @IBOutlet weak var threeOfSpadesButton: UIButton!
+    @IBOutlet weak var eightOfDiamondsButton: UIButton!
+    @IBOutlet weak var tenOfHeartsButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     // TODO: IB actions and code to update UI
+
+    @IBAction func cardTypeTapped(sender: AnyObject) {
+        
+        if sender as! NSObject == fourOfClubsButton {
+            
+            centerLabel.text = "4"
+            upperLeftLabel.text = "♣️"
+            lowerRightLabel.text = "♣️"
+        }
+        
+        if sender as! NSObject == threeOfSpadesButton {
+            
+            centerLabel.text = "3"
+            upperLeftLabel.text = "♠️"
+            lowerRightLabel.text = "♠️"
+        }
+        
+        if sender as! NSObject == eightOfDiamondsButton {
+            
+            centerLabel.text = "8"
+            upperLeftLabel.text = "♦️"
+            lowerRightLabel.text = "♦️"
+        }
+
+        if sender as! NSObject == tenOfHeartsButton {
+            
+            centerLabel.text = "10"
+            upperLeftLabel.text = "♥️"
+            lowerRightLabel.text = "♥️"
+        }
+
+
+        
+    }
 }
